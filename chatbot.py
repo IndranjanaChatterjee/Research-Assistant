62cb2deedbb528962ddcb192fa23c8919c43dab0
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2026-02-15 20:44:19 +0530

    feat:Maintaining_Context_using_list

--- chatbot.py
+++ chatbot.py
@@ -14,10 +14,14 @@ model = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
     temperature=0.7
 )
+chat_history = []
 
 while True:
     user_input=input("Me:")
+    chat_history.append(user_input);
     if user_input == 'exit':
         break;
-    result=model.invoke(user_input)
+    result=model.invoke(chat_history)
+    chat_history.append(result.content)
     print("AI:",result.content)
+print(chat_history)
