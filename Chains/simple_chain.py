ee51e8d6ad7eca8612b89cada4bc8c70847ab3c1
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2026-02-17 21:23:27 +0530

    feat:Chains

---
+++ Chains/simple_chain.py
@@ -0,0 +1,29 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+import os
+from langchain_core.prompts import PromptTemplate,load_prompt
+from langchain_core.output_parsers import StrOutputParser
+load_dotenv()
+
+# Check if API key is set
+if not os.getenv("GOOGLE_API_KEY"):
+    raise ValueError("Please set your GOOGLE_API_KEY in the .env file")
+
+# Initialize the chat model with simple configuration
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0.7
+)
+prompt=PromptTemplate(
+    template='Generate 5 interesting facts about {topic}',
+    input_variables=['topic']
+)
+
+parser=StrOutputParser()
+
+chain = prompt | model | parser;
+
+result=chain.invoke({'topic':'biriyani food'})
+print(result)
+
+chain.get_graph().print_ascii()
